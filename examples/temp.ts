4548e64642db4beb1a36fc591301a5f83af3a605
Scott Cooper <scttcper@gmail.com>
2019-04-17 09:25:51 -0700

    chore: same example as readme

--- examples/temp.ts
+++ examples/temp.ts
@@ -1,23 +1,15 @@
 import { catchCloudflare } from '../src/index';
-import got from 'got';
+import got, { GotOptions } from 'got';
 import { CookieJar } from 'tough-cookie';
-import uaString from 'ua-string';
 
 // example helper function
 async function main() {
-  // cookie jar required
+  // cookie jar is required!
   const cookieJar = new CookieJar();
-  const options: any = {
+  const options: GotOptions<any> = {
     cookieJar,
-    headers: {
-      // helps to pass user-agent
-      'user-agent': uaString,
-    },
-    retry: {
-      // either disable retry or remove status code 503 from retries
-      // retries: 0,
-      statusCodes: [408, 413, 429, 500, 502, 504],
-    },
+    // either disable retry or remove status code 503 from retries
+    retry: 0,
   };
 
   let res: got.Response<any>;

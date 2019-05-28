1c9f2cdb055d85ed778b17c7f0c2f31e10a0f881
Scott Cooper <scttcper@gmail.com>
2019-05-27 21:04:48 -0700

    fix: allow install from github, remove package lock

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 0,
     'capitalized-comments': 0,
     'comma-dangle': ['error', 'always-multiline'],
-    'no-mixed-operators': 0,
-    '@typescript-eslint/promise-function-async': 0,
-    '@typescript-eslint/unified-signatures': 0,
   },
 };
